d14989a187b8a12bb12c16958a81aa37e08afa49
ramesh-tamang <ramesh1042637@gmail.com>
2025-11-14 08:19:28 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.strip() for req in requirements]  # "\n" हटाउने
+        requirements = [req.strip() for req in requirements]  
 
     if HYPEN_E_DOT in requirements:
         requirements.remove(HYPEN_E_DOT)
